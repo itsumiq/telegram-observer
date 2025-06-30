529fb36cc1c17a0261341489457bddb9e64fc949
itsumi <122093200+itsumiq@users.noreply.github.com>
2025-07-01 00:21:36 +0300

    feat(config): add dev configuration

---
+++ internal/infrastructure/config/config.go
@@ -0,0 +1,54 @@
+package config
+
+import (
+	"fmt"
+	"log/slog"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Mode string
+
+const (
+	ModeDevelopment Mode = "dev"
+	ModeProduction  Mode = "prod"
+)
+
+type server struct {
+	Addr        string `yaml:"addr"`
+	Port        int    `yaml:"port"`
+	MaxFileSize int64  `yaml:"max-file-size"`
+}
+
+type telegram struct {
+	Token string `yaml:"token" env:"TELEGRAM_TOKEN" env-required:"true"`
+}
+
+type sqlite struct {
+	DriverName string `yaml:"driver-name"`
+	Path       string `yaml:"path"        env:"SQLITE_PATH" env-required:"true"`
+}
+
+type Config struct {
+	Server   server
+	Telegram telegram
+	Sqlite   sqlite
+}
+
+func New(mode string, log *slog.Logger) (*Config, error) {
+	config := &Config{}
+
+	configPath := "configs/config-dev.yaml"
+	if Mode(mode) == ModeProduction {
+		configPath = "config/config-prod.yaml"
+	}
+
+	if err := cleanenv.ReadConfig(configPath, config); err != nil {
+		log.Error("failed to read config", "error", err, "config_path", configPath)
+		return nil, fmt.Errorf("config.New: %w", err)
+	}
+
+	log.Info("config successful readed")
+
+	return config, nil
+}
