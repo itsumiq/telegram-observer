8e532f01b2dcf7a37404ad7a18786565d443bfe1
itsumi <122093200+itsumiq@users.noreply.github.com>
2025-07-09 19:37:17 +0500

    feat(domain): add SendPhotoMessage method to message.Processing interface

--- internal/domain/message/message_processing.go
+++ internal/domain/message/message_processing.go
@@ -1,5 +1,10 @@
 package message
 
+// Processing represents contract that message processings should implement.
 type Processing interface {
+	// Send sends text message and returns error.
 	Send(msg *Message) error
+
+	// SendPhotoMessage sends photo message and returns error.
+	SendPhotoMessage(msg *Message) error
 }
