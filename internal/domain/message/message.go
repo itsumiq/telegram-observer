9a422d454a38826c3b05963a333cd4d504631216
itsumi <122093200+itsumiq@users.noreply.github.com>
2025-07-09 19:24:09 +0500

    docs(domain): add documentation for message entity

--- internal/domain/message/message.go
+++ internal/domain/message/message.go
@@ -2,6 +2,7 @@ package message
 
 import "strings"
 
+// Message represents  message domain entity.
 type Message struct {
 	ChatID   int64
 	Text     string
@@ -9,20 +10,24 @@ type Message struct {
 	FilePath string
 }
 
+// Button represent Message field.
 type Button struct {
 	Text string
 	Data string
 }
 
+// New creates new message instance.
 func New(chatID int64, text string, filePath string) *Message {
 	text = convertToMarkdown(text)
 	return &Message{ChatID: chatID, Text: text, FilePath: filePath}
 }
 
+// AddButton adds button to message.
 func (m *Message) AddButton(text string, data string) {
 	m.Buttons = append(m.Buttons, Button{Text: text, Data: data})
 }
 
+// convertToMarkdown converts text markup to markdown and returns it.
 func convertToMarkdown(text string) string {
 	specChars := []string{
 		"_",
