c4e3766282488089494d23b84174d3cdbcb9ab72
itsumi <122093200+itsumiq@users.noreply.github.com>
2025-07-09 15:21:13 +0500

    feat: add app initialization

---
+++ internal/app/app.go
@@ -0,0 +1,59 @@
+package app
+
+import (
+	"fmt"
+	"log/slog"
+	httpapp "telegram-observer/internal/app/http"
+	"telegram-observer/internal/application"
+	"telegram-observer/internal/infrastructure/config"
+	messageprocessing "telegram-observer/internal/infrastructure/message_processing"
+	"telegram-observer/internal/infrastructure/storage/sqlite"
+	uuiderclient "telegram-observer/internal/infrastructure/uuider"
+	"telegram-observer/internal/presentation/http/handler"
+)
+
+type App struct {
+	httpApp *httpapp.App
+	log     *slog.Logger
+}
+
+func New(cfg *config.Config, log *slog.Logger) (*App, error) {
+	tgClient, err := messageprocessing.NewTelegramClient(cfg.Telegram.Token, cfg.Telegram.Url, log)
+	if err != nil {
+		return nil, fmt.Errorf("app.New: %w", err)
+	}
+
+	db, err := sqlite.Connect(cfg.Sqlite.DriverName, cfg.Sqlite.Path, cfg.Sqlite.MigrationPath, log)
+	if err != nil {
+		return nil, fmt.Errorf("app.New: %w", err)
+	}
+
+	userRepo := sqlite.NewUserRepository(db)
+	uuider := uuiderclient.New()
+
+	userService := application.NewUserService(userRepo, uuider)
+	messageService := application.NewMessageService(tgClient)
+
+	tgHandler := handler.NewTelegramHandler(userService, messageService)
+	userHandler := handler.NewUserHandler(
+		userService,
+		messageService,
+		cfg.Server.FilePath,
+		cfg.Server.MaxFileSize,
+	)
+
+	httpApp := httpapp.New(cfg.Server.Addr, cfg.Server.Port, tgHandler, userHandler, log)
+
+	return &App{httpApp: httpApp, log: log}, nil
+}
+
+func (a *App) Run() {
+	a.httpApp.Run()
+}
+
+func (a *App) Shutdown() error {
+	if err := a.httpApp.Shutdown(); err != nil {
+		return fmt.Errorf("app.Shutdown: %w", err)
+	}
+	return nil
+}
